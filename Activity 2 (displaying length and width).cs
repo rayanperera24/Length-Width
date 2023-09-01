4a42be435f4c474c8d381b7d6eff2be9a250452a
Rayan Perera <rayanp2004@gmail.com>
2023-09-01 21:27:15 +0530

    Initial commit

---
+++ Activity 2 (displaying length and width).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    internal class Program
+    {
+
+        class rectangle
+        {
+            //attributes
+            private int length=20;
+            private int width=10;
+            public void displaylength()
+            {
+                Console.WriteLine("length " + length);
+            }
+            public void displaywidth()
+            {
+                Console.WriteLine("width " + width);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            rectangle rec1 = new rectangle();
+            rec1.displaylength();
+            rec1.displaywidth();
+
+            Console.ReadKey();
+        }
+
+        
+        
+    }
+
+ 
+}
+
